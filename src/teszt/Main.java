15c21cf79c4ccfaf7b0ac2ba867cc683db8662f5
logn <danyiszabi94@gmail.com>
2016-03-18 21:45:33 +0100

    Szabi: teszt

--- src/teszt/Main.java
+++ src/teszt/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("verified teszt! ");
+		System.out.print("verified teszt! asd");
 
 	}
 

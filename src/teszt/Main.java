6952e12ea8e63b836d4ea5b7f0fbb0d1f23cd43a
Tóth Máté <tothmate0712@gmail.com>
2016-03-18 22:04:30 +0100

    Orok szercsi - T0pi

--- src/teszt/Main.java
+++ src/teszt/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("verified teszt! asd" + "T00pi <3");
+		System.out.print("verified teszt! asd" + "T00pi <3" + "Széki <3");
 
 	}
 

e4f298c7c31a3fa985b29ae0ccd9aebeaae436db
logn <danyiszabi94@gmail.com>
2016-03-18 21:16:35 +0100

    teszt2

--- src/teszt/Main.java
+++ src/teszt/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("Sziasztok! Ez itt a SzL4 teszt szövege!");
+		System.out.print("Sziasztok! Ez itt a SzL4 teszt szövegeee!");
 
 	}
 

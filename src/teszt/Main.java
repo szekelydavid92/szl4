e6b12fb6228c823b206f26578fe9efe6ad450fdd
logn <danyiszabi94@gmail.com>
2016-03-18 21:14:50 +0100

    Teszt

---
+++ src/teszt/Main.java
@@ -0,0 +1,11 @@
+package teszt;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Sziasztok! Ez itt a SzL4 teszt szövege!");
+
+	}
+
+}

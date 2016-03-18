cac78fd1e74bbb0d5e6651affc64050bcea519b3
logn <danyiszabi94@gmail.com>
2016-03-18 21:42:30 +0100

    vereified email teszt

--- src/teszt/Main.java
+++ src/teszt/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("aaaakkkkkkkkkkk!");
+		System.out.print("verified teszt!");
 
 	}
 

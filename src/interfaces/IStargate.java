d3ff30ec6289a68a23e9b154e10849b564bbdbb4
szekelydaivd92 <szekelydavid92@gmail.com>
2016-03-22 17:12:16 +0100

    IStargate interfész implementálása, javadoc. - Széki

--- src/interfaces/IStargate.java
+++ src/interfaces/IStargate.java
@@ -1,5 +1,13 @@
 package interfaces;
 
+/**
+ * Ezt az interface-t implementálja a csillagkapu.
+ */
 public interface IStargate {
-
+	/**
+	 * Ezzel a függvénnyel nyílik meg a csillagkapu egy speciális falon.
+	 * @param wall Egy ISpecWall interfészt implementáló objektum.
+	 * @return void
+	 */
+	public void mask(ISpecWall wall);
 }

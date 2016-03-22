157a7c4da85d5e53b6bcb9f42afee5d2c82daa43
szekelydaivd92 <szekelydavid92@gmail.com>
2016-03-22 17:16:47 +0100

    ITeleportable interfész implementálása, javadoc. - Széki

--- src/interfaces/ITeleportable.java
+++ src/interfaces/ITeleportable.java
@@ -1,5 +1,14 @@
 package interfaces;
 
+/**
+ * Ez az interface felel a teleportálhatóságért.
+ */
 public interface ITeleportable {
-
+	/**
+	 * Ezzel a függvénnyel lehet az implementáló osztály példányát átteleportálni egy másik helyre.
+	 * @param x A cél koordinátájának x tagja.
+	 * @param y A cél koordinátájának y tagja.
+	 * @return void
+	 */
+	public void teleport(double x, double y);
 }

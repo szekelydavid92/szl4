4a7ae22e42a251a526f4669b8c700783f7d043f2
szekelydaivd92 <szekelydavid92@gmail.com>
2016-03-22 17:03:30 +0100

    IKillable interfész implementálása, javadoc. - Széki

--- src/interfaces/IKillable.java
+++ src/interfaces/IKillable.java
@@ -1,5 +1,12 @@
 package interfaces;
 
+/**
+ * Ez az Interface felel a megsemissíthetõségért.
+ */
 public interface IKillable {
-
+	/**
+	 * Ezzel a függvénnyel lehet az interfészt implementáló osztály példányát megsemmisíteni.
+	 * @return void
+	 */
+	public void kill();
 }

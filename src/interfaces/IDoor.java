8308ac3987ea5a0fff425748ecfa66b16e21c1a6
szekelydaivd92 <szekelydavid92@gmail.com>
2016-03-22 16:58:49 +0100

    IDoor interfész implementálása, javadoc. - Széki

--- src/interfaces/IDoor.java
+++ src/interfaces/IDoor.java
@@ -1,5 +1,12 @@
 package interfaces;
 
+/**
+ * Ezen Interface felel az ajtók nyithatóságáért.
+ */
 public interface IDoor {
-
+	/**
+	 * Ez a függvény nyitja ki az ajtót.
+	 * @return void
+	 */
+	public void open();
 }

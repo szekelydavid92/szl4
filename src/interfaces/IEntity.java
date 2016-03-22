c2bec896119c37c1938485545ab171b5f0af22b9
szekelydaivd92 <szekelydavid92@gmail.com>
2016-03-22 17:00:43 +0100

    IEntity interfész implementálása, javadoc. - Széki

--- src/interfaces/IEntity.java
+++ src/interfaces/IEntity.java
@@ -1,5 +1,14 @@
 package interfaces;
 
+/**
+ * Ezen interface-t azon osztályok implementálják, akik értesülni akarnak arról, hogy az idõ telik,
+ * azaz elõre akarják léptetni az állapotukat minden ciklusban.
+ */
 public interface IEntity {
-
+	/**
+	 * Ez az a függvény, amelyet a GameLoop minden ciklusban lehív.
+	 * Ebben a metódusban léptetik elõre az objektumok az állapotukat az idõ függvényében.
+	 * @return void
+	 */
+	public void step();
 }

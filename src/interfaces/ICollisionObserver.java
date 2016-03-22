fefbe4465f328947f1b8ce3f82d70aad749c7007
szekelydaivd92 <szekelydavid92@gmail.com>
2016-03-22 16:56:24 +0100

    ICollisionObserver interfész implementálása és hozzá javadoc. - Széki

--- src/interfaces/ICollisionObserver.java
+++ src/interfaces/ICollisionObserver.java
@@ -1,5 +1,14 @@
 package interfaces;
 
+/**
+ * Ez az Interface felelõs azért, hogy a felsõ rétegbeli objektumok értesüljenek afelõl,
+ * hogy az alsó rétegbeli reprezentációjuk ütközött egy másik objektummal.
+ */
 public interface ICollisionObserver {
-
+	/**
+	 * Ebben a függvényben valósul meg a felsõ rétegbeli objektum értesítése az ütközés bekövetkeztérõl.
+	 * Az obj paraméter az a másik objektum, akivel a feliratkozó WorldObject-je ütközött.
+	 * @return void
+	 */
+	public void notify(IWorldObject obj);
 }

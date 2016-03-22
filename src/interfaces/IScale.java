2f9aa80626fbcf7074a3758020f75b608cc1a8b3
szekelydaivd92 <szekelydavid92@gmail.com>
2016-03-22 17:10:33 +0100

    IScale interfész implementálása, javadoc. - Széki

--- src/interfaces/IScale.java
+++ src/interfaces/IScale.java
@@ -1,5 +1,12 @@
 package interfaces;
 
+/**
+ * Ez az interfész felelõs azért, hogy az õt megvalósító objektum lenyomható legyen.
+ */
 public interface IScale {
-
+	/**
+	 * Ez a függvény jelzi a mérleg számára, hogy súly van rajta.
+	 * @return void
+	 */
+	public void push();
 }

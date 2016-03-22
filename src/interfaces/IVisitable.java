54a7f93b2f78bc9e5017cac2fe6ea11bd141451b
szekelydaivd92 <szekelydavid92@gmail.com>
2016-03-22 17:19:27 +0100

    IVisitable interfész implementálása, javadoc. - Széki

--- src/interfaces/IVisitable.java
+++ src/interfaces/IVisitable.java
@@ -1,5 +1,14 @@
 package interfaces;
 
+/**
+ * A Visitor tervezési minta egyik eleme. Ezen Interface-t implementáló osztály
+ * képes egy IVisitor tudomására hozni, hogy õ milyen típusú.
+ */
 public interface IVisitable {
-
+	/**
+	 * Ez a függvény felelõs azért, hogy a paraméterként kapott IVisitor-rel közölje, hogy milyen típusú.
+	 * @param visitor Az IVisitor interfészt implementáló objektum.
+	 * @return void
+	 */
+	public void accept(IVisitor visitor);
 }

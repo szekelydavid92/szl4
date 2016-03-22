c64fa037ae1efa84fad66a0694f88ddce4bcf76f
szekelydaivd92 <szekelydavid92@gmail.com>
2016-03-22 17:08:36 +0100

    ISpecWall interfész implementálása, javadoc. - Széki

--- src/interfaces/ISpecWall.java
+++ src/interfaces/ISpecWall.java
@@ -1,5 +1,20 @@
 package interfaces;
 
+/**
+ * Ezt az interface-t a speciális fal implementálja.
+ */
 public interface ISpecWall {
-
+	/**
+	 * Ezzel a függvénnyel tudja jelezni egy csillagkapu a speciális falnak, hogy õ megnyílik rajta.
+	 * @param o Az IStargate interfészt megvalósító objektum.
+	 * @return void
+	 */
+	public void replace(IStargate o);
+	
+	
+	/**
+	 * Ezzel a függvénnyel tudja visszaállítani magát a fal, ha a csillagkapu áthelyezõdik.
+	 * @return void
+	 */
+	public void restore();
 }

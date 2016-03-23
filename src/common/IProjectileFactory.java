3b0413630d8639423715d07aa801a8be332446ca
szekelydaivd92 <szekelydavid92@gmail.com>
2016-03-23 13:21:14 +0100

    IProjectileFactory, IWorldObjectFactory implementálás, javadoc. - Széki

--- src/common/IProjectileFactory.java
+++ src/common/IProjectileFactory.java
@@ -1,5 +1,13 @@
 package common;
 
+/**
+ * Ez az Interface felel a lövedék létrehozásáért.
+ */
 public interface IProjectileFactory {
-
+	/**
+	 * Megadott színû lõvedéket hoz létre.
+	 * @param colour Az adott színû enum.
+	 * @return void
+	 */
+	public void createProjectile(Colour colour);
 }
--- src/common/IWorldObjectFactory.java
+++ src/common/IWorldObjectFactory.java
@@ -1,5 +1,15 @@
 package common;
 
+/**
+ * Az õt megvalósító osztályoknak létre kell tudniuk hozniuk WorldObjecteket, adott szélességgel, és hosszúsággal.
+ */
 public interface IWorldObjectFactory {
+	/**
+	 * Objektum létrehozásáért felel.
+	 * @param width Objektum szélessége.
+	 * @param height Objektum magassága.
+	 * @return void
+	 */
+	public void createObject(double width, double height);
 
 }

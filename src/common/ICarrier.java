75572c9bddd7ce48aa1aab19d2892d91aa7d2412
szekelydaivd92 <szekelydavid92@gmail.com>
2016-03-23 17:02:21 +0100

    ICarrier implementálás, javadoc, ez még ki volt maradva. - Széki

--- src/common/ICarrier.java
+++ src/common/ICarrier.java
@@ -1,11 +1,11 @@
 package common;
 
 /**
- * ???????????????????
+ * Box tartalmaz róla referenciát. Ez felel azért h eldobható legyen a box objektum.
  */
 public interface ICarrier {
 	/**
-	 * ???????
+	 * Törli a róla levõ referenciát a Box objektumból.
 	 * @return void
 	 */
 	public void forcedRelease();

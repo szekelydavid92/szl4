013f34cb874e142bbf87cdf2b8856ed69056343c
Beni <bognar.bence@gmail.com>
2016-03-26 22:35:30 +0100

    fff

--- src/bottom_layer/World.java
+++ src/bottom_layer/World.java
@@ -1,21 +1,40 @@
 package bottom_layer;
 
+import java.util.Vector;
+
 /**
  * Ez az az osztály reprezentálja a virtuális világot. Tartarmaz egy WorldObject tömböt.
  * Fõ feladata, hogy a tömb elemeit léptesse az idõben.
  */
 public class World {
-	/**
-	 * Ütközés ellenõrzés.
-	 * @return void
-	 */
-	public void checkCollision() {};
 	
+	Vector<WorldObject> objects;
+	
+	//Ütközés ellenõrzés.
+	public void checkCollision() {
+		WorldObject temp1,temp2;
+		
+		for(int i=0; i<objects.size(); i++){
+			for(int j=0; j<objects.size(); j++){
+			temp1 = objects.elementAt(i);
+			temp2 = objects.elementAt(j);
+			if(temp1.checkCollision(temp2)){
+				temp1.notify(temp2);
+				temp2.notify(temp1);
+				}
+			}
+		}
+	};
 	
 	/**
 	 * Léptetés megvalósítása.
 	 * @param dt ???????????
 	 * @return void
 	 */
-	public void step(double dt) {}
+	public void step(double dt) {
+		
+		for(int i=0; i<objects.size(); i++){
+			objects.elementAt(i).getIVisitable();
+		}
+	}	
 }

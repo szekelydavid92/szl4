b5465f0a4a5a11e4e6e2682d307361708e11ca13
Seres Lóránt <sereslorant@gmail.com>
2016-03-27 21:13:38 +0200

    World ékezeteinek kijavítása

--- src/bottom_layer/World.java
+++ src/bottom_layer/World.java
@@ -1,23 +1,24 @@
 package bottom_layer;
 
-import java.util.Vector;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
- * Ez az az osztály reprezentálja a virtuális világot. Tartarmaz egy WorldObject tömböt.
- * Fõ feladata, hogy a tömb elemeit léptesse az idõben.
+ * Ez az az osztály reprezentalja a virtualis vilagot. Tartarmaz egy WorldObject tombot.
+ * Fo feladata, hogy a tomb elemeit leptesse az idoben.
  */
 public class World {
 	
-	Vector<WorldObject> objects;
+	List<WorldObject> objects = new ArrayList<WorldObject>();
 	
-	//Ütközés ellenõrzés.
+	//Utkozes ellenorzes.
 	public void checkCollision() {
 		WorldObject temp1,temp2;
 		
 		for(int i=0; i<objects.size(); i++){
 			for(int j=0; j<objects.size(); j++){
-			temp1 = objects.elementAt(i);
-			temp2 = objects.elementAt(j);
+			temp1 = objects.get(i);
+			temp2 = objects.get(j);
 			if(temp1.checkCollision(temp2)){
 				temp1.notify(temp2);
 				temp2.notify(temp1);
@@ -27,13 +28,13 @@ public class World {
 	};
 	
 	/**
-	 * Léptetés megvalósítása.
+	 * Leptetes megvalositasa.
 	 * @return void
 	 */
 	public void step() {
 		
 		for(int i=0; i<objects.size(); i++){
-			objects.elementAt(i).step();
+			objects.get(i).step();
 		}
 	}	
 }

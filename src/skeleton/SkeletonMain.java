57e95c2a4b003f966f004ea54c750cefb367f149
Beni <bognar.bence@gmail.com>
2016-03-27 15:23:29 +0200

    case függvények váza

--- src/skeleton/SkeletonMain.java
+++ src/skeleton/SkeletonMain.java
@@ -4,5 +4,54 @@ public class SkeletonMain {
 	public static void main(String[] args) {
 		System.out.print("Tunderpuszcsyka\n");
 		System.out.print("8=====Dn");
+		
+	}
+	//O’Neill mozgatása
+	void case_1(){
+		
+	}
+	//Sárga lövedék lövése
+	void case_2(){
+	
+	}
+	//Kék lövedék lövése
+	void case_3(){
+		
+	}
+	//Sárga csillagkapu nyitása
+	void case_4(){
+		
+	}
+	//Kék csillagkapu nyitása
+	void case_5(){
+		
+	}
+	//Csillagkapu átjárása
+	void case_6(){
+		
+	}
+	//Doboz felvétele
+	void case_7(){
+			
+	}
+	//Doboz lerakása
+	void case_8(){
+		
+	}
+	//ZPM felvétele
+	void case_9(){
+		
+	}
+	//Ajtó nyitása
+	void case_10(){
+			
+	}
+	// Ajtó zárása
+	void case_11(){
+			
+	}
+	//Objektum megsemmisülése
+	void case_12(){
+		
 	}
 }

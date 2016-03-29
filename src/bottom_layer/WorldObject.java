9e02c460ed062420b6e18100759b5af972a6ee37
szekelydaivd92 <szekelydavid92@gmail.com>
2016-03-29 14:38:49 +0200

    WorldObject megjavítása

--- src/bottom_layer/WorldObject.java
+++ src/bottom_layer/WorldObject.java
@@ -19,7 +19,7 @@ import skeleton.Depth;
  */
 public class WorldObject implements IWorldObject {
 	
-	public IVisitable iv;
+	//public IVisitable iv;
 	public String name = "worldObj"; //O kell az objektum nevenek a kiprintelesehez!
 	
 	/* Ez az objektum felelos azert, hogy
@@ -392,8 +392,8 @@ public class WorldObject implements IWorldObject {
 		Depth.getInstance().printTabs();
 		System.out.print("ret " + name + ".getVisitable()\n");
 		
-		//return visitable;
-		return iv;
+		return visitable;
+		//return iv;
 	}
 
 }

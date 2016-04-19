5fe15ae1f327a6da268d3b82412f08effa9e1e53
Seres Lóránt <sereslorant@gmail.com>
2016-04-19 22:29:51 +0200

    IProtoCommand feltöltése

---
+++ src/proto/IProtoCommand.java
@@ -0,0 +1,11 @@
+package proto;
+
+import java.util.Scanner;
+
+/**
+ * Az oka annak, hogy ez NEM az interfaces package-be kerul, az az, hogy kerdeses, hogy bennemarad-e a
+ * vegso programban, es ha igen, akkor is egy masik absztrakcios retegben van a helye, mint a modellnek.
+ */
+public interface IProtoCommand {
+	public String Execute(Scanner in);
+}

9e3ba6bb4b19f983e4cc61942c16b285a8cc6567
szekelydaivd92 <szekelydavid92@gmail.com>
2016-04-23 00:27:34 +0200

    Replicator step fvének és setActive fvének megírása. - Széki

--- src/upper_layer/entity/ReplicatorController.java
+++ src/upper_layer/entity/ReplicatorController.java
@@ -5,14 +5,55 @@ import common.IEntity;
 public class ReplicatorController implements IEntity {
 	private int direction;
 	private boolean active;
+	private Replicator replicator;
+	
+	public void setReplicator(Replicator r) {
+		replicator = r;
+	}
 	
 	@Override
 	public void step() {
-		// TODO Auto-generated method stub
-		
+		if (active) {
+			switch (direction) {
+				case 0:
+					replicator.moveUp(true);
+					break;
+				case 1:
+					replicator.moveLeft(true);
+					replicator.moveUp(true);
+					break;
+				case 2:
+					replicator.moveLeft(true);
+					break;
+				case 3:
+					replicator.moveLeft(true);
+					replicator.moveDown(true);
+					break;
+				case 4:
+					replicator.moveDown(true);
+					break;
+				case 5:
+					replicator.moveDown(true);
+					replicator.moveRight(true);
+					break;
+				case 6:
+					replicator.moveRight(true);
+					break;
+				case 7:
+					replicator.moveRight(true);
+					replicator.moveUp(true);
+					break;
+			}
+		}
+		else {
+			replicator.moveLeft(false);
+			replicator.moveUp(false);
+			replicator.moveDown(false);
+			replicator.moveRight(false);
+		}	
 	}
 	
 	public void setActive(boolean active) {
-		
+		this.active = active;
 	}
 }

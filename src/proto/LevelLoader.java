842635682ff439217e7c628aafb6e399472eb74d
Seres Lóránt <sereslorant@gmail.com>
2016-04-24 15:25:53 +0200

    LevelLoader kommentezése

--- src/proto/LevelLoader.java
+++ src/proto/LevelLoader.java
@@ -7,16 +7,21 @@ import java.util.Scanner;
 import bottom_layer.GameLoop;
 import upper_layer.GameFactory;
 
-/*
- * TODO Meg kell csinálni a door és a scale betöltőjét! MEG VAN
- * TODO Meg kell csinálni a ZPM_RandPos betöltőjét! MEG VAN
+/**
+ * @brief Ez az osztaly felelos a palya betolteseert.
  */
-
 class LevelLoader {
 	
+	/**
+	 * @brief Ez az osztaly felelos a parameterkent kapott Scannerbol a palya elemeinek
+	 * a beolvasasaert.
+	 */
 	static class LevelParser extends CommandInterpreter {
 		private GameFactory gameFactory;
 		
+		/**
+		 * @brief Ez az osztaly felelos egy konkret palyaelem betolteseert.
+		 */
 		static abstract class ObjectParser implements IProtoCommand {
 			
 			enum State {
@@ -30,9 +35,31 @@ class LevelLoader {
 				this.levelParser = levelParser;
 			}
 			
+			/**
+			 * @brief Ez a fuggveny felelos egy konkret tulajdonsag beolvasasaert.
+			 * 
+			 * @param name az attributum neve
+			 * @param in a bemenet, ahonnan beolvassuk az attributum ertekeit.
+			 */
 			abstract protected void parseAttrib(String name,Scanner in);
+			
+			/**
+			 * @brief Ez a fuggveny felelos az objektum GameFactoryn keresztul torteno
+			 * megkonstrualasaert.
+			 * 
+			 * @return bool true, ha minden letfontossagu attributum betoltodott, es
+			 * sikerult megkonstrualni az objektumot, false egyebkent.
+			 */
 			abstract protected boolean Construct();
 			
+			/**
+			 * @brief Ez a fuggveny futtatja az allapotgepet, ami beolvassa a palyaelemet.
+			 * 
+			 * @param in egy Scanner, ami csomagolja a bemenetet. Innen olvassuk be a palyaelemet
+			 * 
+			 * @return boolean - true, ha sikerrel beolvastuk es megkonstrualtuk
+			 * a palyaelemet, false egyebkent.
+			 */
 			@Override
 			public boolean Execute(Scanner in) {
 				boolean isRunning = true;
@@ -62,6 +89,10 @@ class LevelLoader {
 			}
 		}
 		
+		/**
+		 * @brief Ez az osztaly felelos a pozicioval es szelesseggel meg hosszusaggal
+		 * rendelkezo objektumok betolteseert.
+		 */
 		static abstract class PosDimParser extends ObjectParser {
 
 			boolean posLoaded = false;
@@ -73,6 +104,12 @@ class LevelLoader {
 				super(levelParser);
 			}
 			
+			/**
+			 * @brief Ez a fuggveny felelos a pozicio es a szelesseg beolvasasaert.
+			 * 
+			 * @param name az attributum neve
+			 * @param in a bemenet, ahonnan beolvassuk az attributum ertekeit.
+			 */
 			@Override
 			protected void parseAttrib(String name, Scanner in) {
 				if(name.equals("pos")) {
@@ -88,6 +125,9 @@ class LevelLoader {
 			
 		};
 		
+		/**
+		 * @brief Ez az objektum felelos az ajto beolvasasaert.
+		 */
 		class DoorParser extends PosDimParser{
 			
 			int id;
@@ -96,7 +136,13 @@ class LevelLoader {
 			DoorParser(LevelParser levelParser) {
 				super(levelParser);
 			}
-			
+
+			/**
+			 * @brief Ez a fuggveny felelos az id beolvasasaert.
+			 * 
+			 * @param name az attributum neve
+			 * @param in a bemenet, ahonnan beolvassuk az attributum ertekeit.
+			 */
 			@Override
 			protected void parseAttrib(String name, Scanner in) {
 				super.parseAttrib(name, in);
@@ -105,7 +151,13 @@ class LevelLoader {
 					idLoaded = true;
 				}
 			}
-
+			
+			/**
+			 * @brief Ez a fuggveny felelos az ajto megkonstrualasaert.
+			 * 
+			 * @return bool true, ha minden letfontossagu attributum betoltodott, es
+			 * sikerult megkonstrualni az objektumot, false egyebkent.
+			 */
 			@Override
 			protected boolean Construct() {
 				if(posLoaded && dimLoaded && idLoaded) {
@@ -116,7 +168,10 @@ class LevelLoader {
 				return false;
 			}
 		}
-
+		
+		/**
+		 * @brief Ez az objektum felelos a merleg beolvasasaert.
+		 */
 		class ScaleParser extends DoorParser{
 			
 			boolean massLoaded = false;
@@ -124,7 +179,14 @@ class LevelLoader {
 			
 			ScaleParser(LevelParser levelParser) {
 				super(levelParser);
-			}	
+			}
+
+			/**
+			 * @brief Ez a fuggveny felelos a maximalis tomeg beolvasasaert.
+			 * 
+			 * @param name az attributum neve
+			 * @param in a bemenet, ahonnan beolvassuk az attributum ertekeit.
+			 */
 			@Override
 			protected void parseAttrib(String name, Scanner in) {
 				super.parseAttrib(name, in);
@@ -134,6 +196,12 @@ class LevelLoader {
 				}
 			}
 			
+			/**
+			 * @brief Ez a fuggveny felelos a merleg megkonstrualasaert.
+			 * 
+			 * @return bool true, ha minden letfontossagu attributum betoltodott, es
+			 * sikerult megkonstrualni az objektumot, false egyebkent.
+			 */
 			@Override
 			protected boolean Construct() {
 				if(posLoaded && dimLoaded && massLoaded && idLoaded) {
@@ -146,6 +214,9 @@ class LevelLoader {
 			
 		}
 		
+		/**
+		 * @brief Ez az objektum felelos a doboz beolvasasaert.
+		 */
 		class BoxParser extends PosDimParser {
 			
 			boolean massLoaded = false;
@@ -154,7 +225,13 @@ class LevelLoader {
 			BoxParser(LevelParser levelParser) {
 				super(levelParser);
 			}
-			
+
+			/**
+			 * @brief Ez a fuggveny felelos a tomeg beolvasasaert.
+			 * 
+			 * @param name az attributum neve
+			 * @param in a bemenet, ahonnan beolvassuk az attributum ertekeit.
+			 */
 			@Override
 			protected void parseAttrib(String name, Scanner in) {
 				super.parseAttrib(name, in);
@@ -164,6 +241,12 @@ class LevelLoader {
 				}
 			}
 
+			/**
+			 * @brief Ez a fuggveny felelos a doboz megkonstrualasaert.
+			 * 
+			 * @return bool true, ha minden letfontossagu attributum betoltodott, es
+			 * sikerult megkonstrualni az objektumot, false egyebkent.
+			 */
 			@Override
 			protected boolean Construct() {
 				if(posLoaded && dimLoaded && massLoaded) {
@@ -174,7 +257,10 @@ class LevelLoader {
 				return false;
 			}
 		}
-			
+		
+		/**
+		 * @brief Ez az objektum felelos a jatekos beolvasasaert.
+		 */
 		class PlayerParser extends BoxParser {
 			boolean nameLoaded = false;
 			String name;
@@ -182,7 +268,13 @@ class LevelLoader {
 			PlayerParser(LevelParser levelParser) {
 				super(levelParser);
 			}
-			
+
+			/**
+			 * @brief Ez a fuggveny felelos a player nevenek a beolvasasaert.
+			 * 
+			 * @param name az attributum neve
+			 * @param in a bemenet, ahonnan beolvassuk az attributum ertekeit.
+			 */
 			@Override
 			protected void parseAttrib(String name, Scanner in) {
 				super.parseAttrib(name, in);
@@ -192,6 +284,12 @@ class LevelLoader {
 				}
 			}
 
+			/**
+			 * @brief Ez a fuggveny felelos a jatekos megkonstrualasaert.
+			 * 
+			 * @return bool true, ha minden letfontossagu attributum betoltodott, es
+			 * sikerult megkonstrualni az objektumot, false egyebkent.
+			 */
 			@Override
 			protected boolean Construct() {
 				if(posLoaded && dimLoaded && massLoaded && nameLoaded) {
@@ -202,13 +300,22 @@ class LevelLoader {
 				return false;
 			}
 		}
-
+		
+		/**
+		 * @brief Ez az objektum felelos a szakadek beolvasasaert.
+		 */
 		static class ChasmParser extends PosDimParser {
 
 			ChasmParser(LevelParser levelParser) {
 				super(levelParser);
 			}
 
+			/**
+			 * @brief Ez a fuggveny felelos a szakadek megkonstrualasaert.
+			 * 
+			 * @return bool true, ha minden letfontossagu attributum betoltodott, es
+			 * sikerult megkonstrualni az objektumot, false egyebkent.
+			 */
 			@Override
 			protected boolean Construct() {
 				if(posLoaded && dimLoaded) {
@@ -220,12 +327,21 @@ class LevelLoader {
 			}
 		}
 		
+		/**
+		 * @brief Ez az objektum felelos a fal beolvasasaert.
+		 */
 		static class WallParser extends PosDimParser {
 
 			WallParser(LevelParser levelParser) {
 				super(levelParser);
 			}
 
+			/**
+			 * @brief Ez a fuggveny felelos a fal megkonstrualasaert.
+			 * 
+			 * @return bool true, ha minden letfontossagu attributum betoltodott, es
+			 * sikerult megkonstrualni az objektumot, false egyebkent.
+			 */
 			@Override
 			protected boolean Construct() {
 				if(posLoaded && dimLoaded) {
@@ -238,12 +354,21 @@ class LevelLoader {
 			
 		};
 		
+		/**
+		 * @brief Ez az objektum felelos a specialis fal beolvasasaert.
+		 */
 		static class SpecWallParser extends PosDimParser {
 
 			SpecWallParser(LevelParser levelParser) {
 				super(levelParser);
 			}
 
+			/**
+			 * @brief Ez a fuggveny felelos a specialis fal megkonstrualasaert.
+			 * 
+			 * @return bool true, ha minden letfontossagu attributum betoltodott, es
+			 * sikerult megkonstrualni az objektumot, false egyebkent.
+			 */
 			@Override
 			protected boolean Construct() {
 				if(posLoaded && dimLoaded) {
@@ -255,12 +380,21 @@ class LevelLoader {
 			}
 		}
 		
+		/**
+		 * @brief Ez az objektum felelos a ZPM beolvasasaert.
+		 */
 		static class ZPMParser extends PosDimParser {
 
 			ZPMParser(LevelParser levelParser) {
 				super(levelParser);
 			}
 
+			/**
+			 * @brief Ez a fuggveny felelos a ZPM megkonstrualasaert.
+			 * 
+			 * @return bool true, ha minden letfontossagu attributum betoltodott, es
+			 * sikerult megkonstrualni az objektumot, false egyebkent.
+			 */
 			@Override
 			protected boolean Construct() {
 				if(posLoaded && dimLoaded) {
@@ -272,12 +406,21 @@ class LevelLoader {
 			}
 		}
 		
+		/**
+		 * @brief Ez az objektum felelos a ZPM random poziciojanak beolvasasaert.
+		 */
 		class ZPM_RandPosParser extends PosDimParser{
 
 			ZPM_RandPosParser(LevelParser levelParser) {
 				super(levelParser);
 			}
 
+			/**
+			 * @brief Ez a fuggveny felelos a ZPM random poziciojanak megkonstrualasaert.
+			 * 
+			 * @return bool true, ha minden letfontossagu attributum betoltodott, es
+			 * sikerult megkonstrualni az objektumot, false egyebkent.
+			 */
 			@Override
 			protected boolean Construct() {
 				if(posLoaded && dimLoaded) {
@@ -309,7 +452,16 @@ class LevelLoader {
 	private GameFactory gameFactory;
 	
 	/*
-	 * Itt elterunk a specifikaciotol, itt parameterkent kap egy gameLoopot
+	 * Itt elterunk a specifikaciotol, itt parameterkent kap egy gameFactoryt
+	 */
+	
+	/**
+	 * @brief Ez a fuggveny felelos az utvonalon talalhato fajlbol a palya betolteseert.
+	 * 
+	 * @param path a palya eleresi utvonala
+	 * @param gameFactory a GameFactory, amelyen keresztul letrehozzuk a palyaelemeket
+	 * 
+	 * @return true, ha sikerult megnyitni a fajlt, false egyebkent
 	 */
 	public boolean load(String path,GameFactory gameFactory) {
 		this.gameFactory=gameFactory;

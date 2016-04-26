582633554430118e57f25635b9d800909e13f6ca
szekelydaivd92 <szekelydavid92@gmail.com>
2016-04-26 14:37:45 +0200

    komment

--- src/common/IReplicator.java
+++ src/common/IReplicator.java
@@ -1,5 +1,12 @@
 package common;
 
+/**
+ * Ez az Interface felel a Replikatorert.
+ */
 public interface IReplicator {
+	/**
+	 * @brief Meg lehet olni a replikatort ezen a fuggvenyen keresztul.
+	 * @return void
+	 */
 	public void projectileKill();
 }

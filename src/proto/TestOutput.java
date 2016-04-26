d6f2258619f977ee3e7d578e4d2153fa52aca2f8
szekelydaivd92 <szekelydavid92@gmail.com>
2016-04-26 11:18:11 +0200

    Javitas

--- src/proto/TestOutput.java
+++ src/proto/TestOutput.java
@@ -8,55 +8,17 @@ import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class TestOutput {
-	// Argumentum n�lk�l mindent �sszehasonl�t.
-		// 1 argumentum eset�n meg kell adni egy f�jlnevet.
 		public static void main(String[] args) {
-//			if (args.length == 0) {
-//				int testFileCount = new File("expected/").listFiles().length;
-//				for(int i = 0; i != testFileCount; ++i) {
-//					try {
-//						int counter = 0;
-//						boolean fail = false;
-//						BufferedReader expectedReader = new BufferedReader(new FileReader("expected/test" + String.valueOf(i+1) + ".txt"));
-//						BufferedReader outReader = new BufferedReader(new FileReader("out/test" + String.valueOf(i+1) + ".txt"));
-//						System.out.println(String.valueOf(i+1) + ". teszteset futtatasa:");
-//						System.out.println("---------------------------------------");
-//						String expected = null;
-//						while( (expected = expectedReader.readLine()) != null) {
-//							if (expected.equals(outReader.readLine()))
-//								++counter;
-//							else {
-//								fail = true;
-//								System.out.println(String.valueOf(counter+1) + ". sor: nem egyezik a kimenet az elvart kimenettel!");
-//							}
-//						}
-//						if (fail)
-//							System.out.println("A teszt eredmenye: helytelen kimenet.");
-//						else if (outReader.readLine() != null)
-//							System.out.println("A teszt eredmenye: helytelen kimenet.");
-//						else
-//							System.out.println("A teszt eredmenye: helyes kimenet.");
-//						expectedReader.close();
-//						outReader.close();
-//						System.out.println();
-//						BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//						br.readLine();
-//					}		
-//					catch (IOException e) {
-//						System.out.println("Hibas bemeneti fajlok.");
-//					}
-//				}
-//			}
 			if (args.length == 0) {
 				Scanner sc = new Scanner(System.in);
-				System.out.println("Add meg az ellenőrizendő kimeneti tesztesetet: ");
+				System.out.println("Add meg az ellenőrizendő kimeneti teszteset számát: ");
 				String testNumber = sc.nextLine();
 				try {
 					int counter = 0;
 					boolean fail = false;
-					BufferedReader expectedReader = new BufferedReader(new FileReader("expectedOutputs/testOut" + testNumber));//args[0]));
-					BufferedReader outReader = new BufferedReader(new FileReader("output/testOut" + testNumber));//args[0]));
-					System.out.println(/*args[0]*/ testNumber  + ". teszteset futtatasa:");
+					BufferedReader expectedReader = new BufferedReader(new FileReader("expectedOutputs/testOut" + testNumber));
+					BufferedReader outReader = new BufferedReader(new FileReader("output/testOut" + testNumber));
+					System.out.println(testNumber  + ". teszteset futtatasa:");
 					System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
 					String expected = null;
 					while( (expected = expectedReader.readLine()) != null) {

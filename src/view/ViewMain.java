fbf283e0f31a1fbba174ca75b9f271f0f87c1d8a
beni <beni@beni-PC>
2016-05-10 16:24:53 +0200

    View Test

--- src/view/ViewMain.java
+++ src/view/ViewMain.java
@@ -1,10 +1,29 @@
 package view;
 
+import java.awt.Color;
+import java.awt.Graphics;
+
+import bottom_layer.World;
+import bottom_layer.WorldObjectFactory;
+import upper_layer.entity.Player;
+
 public class ViewMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		View v = new View();
+		
+		World world = new World();
+		WorldObjectFactory wof = new WorldObjectFactory(world);
+		
+		//Oneill megjelemítése
+		wof.createObject(10, 10);
+		Player Oneill = new Player(1 ,world.getWorldObject(0));
+		Drawable draPla = new Drawable(Oneill.getWorldObject(), Color.CYAN);	
+		v.add(draPla);
+		
+		v.repaint();
 	}
 
 }

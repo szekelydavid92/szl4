30d935f1d3e5c1e3d1e69e72828e0f1d2996320c
logn <danyiszabi94@gmail.com>
2016-05-16 10:44:17 +0200

    asd

--- src/view/ControllerWhiteBoard.java
+++ src/view/ControllerWhiteBoard.java
@@ -5,7 +5,7 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
-
+ 
 import common.IControllable;
 
 public class ControllerWhiteBoard implements KeyListener, MouseListener {
